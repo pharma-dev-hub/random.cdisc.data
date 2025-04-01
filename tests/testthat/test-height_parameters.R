63665a96a59181aecb9eb0a32655161e78bb3a6e
Iolo <iolo.williams@veramed.co.uk>
2025-04-01 11:43:23 +0100

    Updating the unit tests for the addition of the height argument. ADSL metadata updated so HEIGHT metadata is applied to the adsl dataset. Data snapshot also updated.

--- tests/testthat/test-height_parameters.R
+++ tests/testthat/test-height_parameters.R
@@ -1,31 +1,37 @@
 test_that("height parameters work as expected", {
 
-  # generate adsl without specifying height parameters
-  adsl_default <- radsl()
-
-  # expect the dimension to be 400x55
-  expect_equal(dim(adsl_default), c(400, 55))
-
-  # expect that HEIGHT does not exist
-  expect_false("HEIGHT" %in% names(adsl_default))
-
-
-  # generate adsl with height
-  adsl_height <- radsl(height_male = 180, height_female = 160, height_sd = 8)
+  # generate adsl
+  adsl <- radsl()
 
   # expect the dimension to be 400x56
-  expect_equal(dim(adsl_height), c(400, 56))
+  expect_equal(dim(adsl), c(400, 56))
 
   # expect that HEIGHT exists
-  expect_true("HEIGHT" %in% names(adsl_height))
+  expect_true("HEIGHT" %in% names(adsl))
 
-})
 
-test_that("radsl stops if height parameter lengths are not all the same", {
+  # by default, the mean male height is 180
+  adsl_male <- adsl %>%
+    filter(SEX == "M") %>%
+    select(HEIGHT)
 
-  # expect an error if height parameters lengths are not equal
-  expect_error(radsl(height_male = 180, height_female = 160),
-               "height_male, height_female and height_sd must all be the same length")
+  # expect mean male height is 180 (+ tolerance)
+  expect_equal(mean(as.numeric(adsl_male$HEIGHT)), 180, tolerance = 0.05)
+
+  # by default, the mean male height is 180
+  adsl_female <- adsl %>%
+    filter(SEX == "F") %>%
+    select(HEIGHT)
+
+  # expect mean female height is 160 (+ tolerance)
+  expect_equal(mean(as.numeric(adsl_female$HEIGHT)), 160, tolerance = 0.05)
+
+
+  # expect HEIGHT label is "Height"
+  expect_equal(attr(adsl$HEIGHT, "label"), "Height")
+
+  # expect HEIGHT type is "character"
+  expect_equal(typeof(adsl$HEIGHT), "character")
 
 })
 
